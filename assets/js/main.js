3ed5210ff6c20018a049e643b06ee80cd47103b2
Agh-Hayk <agh.hayk97@mail.ru>
2023-02-07 17:23:30 +0400

    added owl

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:5,
+        items:1,
+        responsive:{
+            1279:{
+                items:2
+            }
+        }
+    });
+  });
